e54a702898883d79c921c2fa806d91d258d5a558
mks_min <mininname@vk.com>
2026-01-04 21:25:47 +0300

    Create first functions

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func calculateIMT(userWeight float64, userHeight float64) float64 {
+	IMT := userWeight / math.Pow(userHeight, 2)
+	return IMT
+}
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("___ Калькулятор индекса массы тела ___")
+	weight, height := userInput()
+
+	imt := calculateIMT(weight, height/100)
+	fmt.Printf("Твой ИМТ: %.2f", imt)
+}
+
+func userInput() (float64, float64) {
+	fmt.Print("Введи свой вес в кг: ")
+	var weight float64
+	fmt.Scan(&weight)
+
+	fmt.Print("Введи свой рост в сантиметрах: ")
+	var height float64
+	fmt.Scan(&height)
+
+	return weight, height
 }
